0048d4f0bacb02eeddbd8f617c86c0cdffb73f80
PrAsAdChAnDwAdKaR <prasadchandwadkar95@gmail.com>
2024-01-17 17:38:19 +0530

    domProject

---
+++ domproject1/app.js
@@ -0,0 +1,24 @@
+const emoji = document.querySelector(".emoji");
+
+const emojis = [
+  "😬",
+  "🤯",
+  "😮",
+  "🙃",
+  "🥱",
+  "😪",
+  "🤩",
+  "😍",
+  "😗",
+  "😶‍🌫️",
+  "🫡",
+  "🥲",
+  "😎",
+  "😂",
+  "😃",
+  "😁",
+]
+
+emoji.addEventListener("mouseover",()=>{
+  emoji.innerHTML= emojis[Math.floor(Math.random()*emojis.length)];
+})
